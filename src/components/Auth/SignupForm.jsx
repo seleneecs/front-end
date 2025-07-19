9644ffc13ae10e4d60eab9ec02e3b38f39ff73e2
selene <selineecl25@gmail.com>
2025-07-19 08:08:55 +0300

    signup with regex

--- src/components/Auth/SignupForm.jsx
+++ src/components/Auth/SignupForm.jsx
@@ -10,7 +10,7 @@ const SignupForm = ({ formData, setFormData, handleSubmit, loading }) => {
     <form onSubmit={handleSubmit}>
       {/* Name Field */}
       <div className="mb-3">
-        <label htmlFor="Name">Name</label>
+        <label htmlFor="Name">Full Names</label>
         <input
           type="text"
           id="Name"
@@ -19,8 +19,11 @@ const SignupForm = ({ formData, setFormData, handleSubmit, loading }) => {
           value={formData.Name}
           onChange={handleChange}
           required
+          pattern="^[A-Za-z\s]+$"
+          title="Only letters and spaces are allowed"
         />
       </div>
+
 
       {/* Phone Field */}
       <div className="mb-3">
@@ -29,12 +32,16 @@ const SignupForm = ({ formData, setFormData, handleSubmit, loading }) => {
           type="text"
           id="Phone"
           name="Phone"
+          placeholder="07xxxxxxxx or 01xxxxxxxx"
           className="form-control"
           value={formData.Phone}
           onChange={handleChange}
           required
+          pattern="^(07|01)\d{8}$"
+          title="Phone number must start with 07 or 01 and be 10 digits long"
         />
       </div>
+
 
       <button type="submit" className="btn btn-success w-100" disabled={loading}>
         {loading ? "Signing up..." : "Sign Up / Register"}
